8db04db8c683e9c76c7050c5191ebe0633625819
Rixu324 <ryuma.miya324@gmail.com>
2024-12-06 15:13:36 +0900

    add Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
