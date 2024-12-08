3e59fe1ebea15e24c0a2a55dffe3c57ac4960508
Rixu324 <ryuma.miya324@gmail.com>
2024-12-09 02:52:17 +0900

    add Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import "./index.css";
 import App from "./App.tsx";
 import React from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Analytics />
     <SpeedInsights />
     <App />
   </React.StrictMode>
